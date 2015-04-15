e93ebd9ee09a6d11d01183405f78988d492e7ad1
Enoch Shin <seekpeace@gmail.com>
2015-04-15 04:30:07 -0400

    hogwarts finished

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+  end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Hogwarts::Application.routes.draw do
- 
-root "house#index"
-resource :house, only: [:index, :show]
-resources :students, only: [:index, :show]
 
+root "houses#index"
+get '/students', :to => 'students#index'
+get '/students/:id', :to => 'students#show', as: 'student'
+get '/students/new', :to => 'students#new'
+get '/new_student', :to => 'students#new'
+resources :houses, only: [:index, :show] do
+resources :students, only: [:index, :show]
+end
 end
